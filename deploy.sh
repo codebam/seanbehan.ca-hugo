fa0bd28c159e996dd30ae21cc677953b276e8cc8
Sean Behan <codebam@riseup.net>
2020-12-04 13:56:13 -0500

    deploy.sh: publish locally on ipfs
    updated script to publish to ipfs locally as well as remotely

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,9 @@ hugo && rsync -r public/ codebam@seanbehan.dev:/usr/share/caddy/seanbehan.dev
 echo publishing to ipfs
 # rsync ipfs_publish.sh codebam@seanbehan.dev:ipfs_publish.sh
 # ssh -t codebam@seanbehan.dev "chmod +x /home/codebam/ipfs_publish.sh"
+
+# publish locally
+ipfs add -r public/
+
+# publish remotely
 ssh -t codebam@seanbehan.dev "sh -c /home/codebam/ipfs_publish.sh" &
