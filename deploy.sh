4e4f0f0b3481eb71566b29ed1c235ca1996d190e
Sean Behan <codebam@riseup.net>
2020-12-04 15:01:41 -0500

    Update deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo publishing to www
-hugo && rsync -r public/ codebam@seanbehan.dev:/usr/share/caddy/seanbehan.dev
+hugo && rsync -r public/ codebam@seanbehan.dev:/var/www/htdocs/seanbehan.dev
 
 echo publishing to ipfs
 # rsync ipfs_publish.sh codebam@seanbehan.dev:ipfs_publish.sh
@@ -11,4 +11,4 @@ echo publishing to ipfs
 ipfs add -r public/
 
 # publish remotely
-ssh -t codebam@seanbehan.dev "sh -c /home/codebam/ipfs_publish.sh" &
+ssh -t codebam@seanbehan.dev "screen -d -m sh -c /home/codebam/ipfs_publish.sh" &
--- ipfs_publish.sh
+++ ipfs_publish.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-result=$(ipfs add -r /usr/share/caddy/seanbehan.dev)
+result=$(ipfs add -r /var/www/htdocs/seanbehan.dev)
 ipfs_hash=$(echo $result | rev | cut -d ' ' -f 2 | rev)
 ipfs name publish --key=seanbehan.dev $ipfs_hash
