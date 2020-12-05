0f6929b86dbcf9d84f3e1f712375c3ac3a71cd33
Sean Behan <codebam@riseup.net>
2020-12-05 03:10:43 -0500

    Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 echo publishing to www
-hugo && rsync -r public/ codebam@seanbehan.dev:/var/www/htdocs/seanbehan.dev
+rm -rf public && hugo && rsync -r public/ codebam@seanbehan.dev:/var/www/htdocs/seanbehan.dev
 
 echo publishing to ipfs
 # rsync ipfs_publish.sh codebam@seanbehan.dev:ipfs_publish.sh
 # ssh -t codebam@seanbehan.dev "chmod +x /home/codebam/ipfs_publish.sh"
 
 # publish locally
-ipfs add -r public/
+ipfs_hash=$(echo $(ipfs add -r public/) | rev | cut -d ' ' -f 2 | rev)
 
 # publish remotely
 ssh -t codebam@seanbehan.dev "screen -d -m sh -c /home/codebam/ipfs_publish.sh" &
