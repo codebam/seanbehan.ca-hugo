0ca78db4f4225668444fa75e85792965fc926661
Sean Behan <codebam@riseup.net>
2021-09-04 16:12:05 -0400

    remove ipfs deploy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
 
 echo publishing to www
-rm -rf public && hugo && rsync -r public/ codebam@seanbehan.dev:/var/www/htdocs/seanbehan.dev --delete
-
-echo publishing to ipfs
-# rsync ipfs_publish.sh codebam@seanbehan.dev:ipfs_publish.sh
-# ssh -t codebam@seanbehan.dev "chmod +x /home/codebam/ipfs_publish.sh"
-
-# publish locally
-ipfs_hash=$(echo $(ipfs add -r public/) | rev | cut -d ' ' -f 2 | rev)
-
-# publish remotely
-ssh -t codebam@seanbehan.dev "screen -d -m sh -c /home/codebam/ipfs_publish.sh" &
+rm -rf public && hugo && rsync -r public/ codebam@seanbehan.ca:/var/www/htdocs/seanbehan.ca --delete
--- ipfs_publish.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-result=$(ipfs add -r /var/www/htdocs/seanbehan.dev)
-ipfs_hash=$(echo $result | rev | cut -d ' ' -f 2 | rev)
-ipfs name publish --key=seanbehan.dev $ipfs_hash
