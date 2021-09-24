47acb1db43a8c6b610b878b7c2a2727db9b39065
Sean Behan <codebam@riseup.net>
2021-09-24 17:41:06 -0400

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo publishing to www
-rm -rf public && hugo && rsync -r public/ codebam@seanbehan.ca:/var/www/htdocs/seanbehan.ca --delete
+rm -rf public && hugo && rsync -r public/ codebam@ssh.seanbehan.ca:~/seanbehan.ca --delete
