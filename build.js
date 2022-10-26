4ecc48a43f6770346afc35722ab0258c702f6f31
Sean Behan <codebam@riseup.net>
2022-10-26 04:16:40 -0400

    use npm to build with hugo and esbuild

---
+++ build.js
@@ -0,0 +1,13 @@
+import esbuild from "esbuild";
+import sveltePlugin from "esbuild-svelte";
+
+esbuild.build({
+  entryPoints: ["src/index.ts"],
+  mainFields: ["svelte", "browser", "module", "main"],
+  bundle: true,
+  outdir: "public/js",
+  plugins: [sveltePlugin()],
+  logLevel: "info",
+  minify: true,
+  sourcemap: true,
+});
