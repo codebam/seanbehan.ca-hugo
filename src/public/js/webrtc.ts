f1f88076e4cfcfed2daf7e721c8677851bafba9b
Sean Behan <codebam@riseup.net>
2022-10-27 07:13:26 -0400

    switch to cloudflare whepclient

--- src/public/js/webrtc.ts
+++ src/public/js/webrtc.ts
@@ -1,13 +1,6 @@
-import { WebRTCPlayer } from "@eyevinn/webrtc-player";
+import WHEPClient from "./WHEPClient";
 
-const player = new WebRTCPlayer({
-  video: document.querySelector("video"),
-  type: "whep",
-  statsTypeFilter: "^candidate-*|^inbound-rtp",
-});
-
-await player.load(
-  new URL(
-    "https://customer-nqr1cy6xpbbv13gu.cloudflarestream.com/817583bd076706ef81bc485f7dd4ec58/webRTC/play"
-  )
+const player = new WHEPClient(
+  "https://customer-nqr1cy6xpbbv13gu.cloudflarestream.com/817583bd076706ef81bc485f7dd4ec58/webRTC/play",
+  document.querySelector("video")
 );
