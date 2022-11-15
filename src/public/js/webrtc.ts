88d933155b99768b154ceb7aada047c739a86aab
Sean Behan <codebam@riseup.net>
2022-11-15 02:43:39 -0500

    switch playback url to use srs

--- src/public/js/webrtc.ts
+++ src/public/js/webrtc.ts
@@ -1,6 +1,6 @@
 import WHEPClient from "./WHEPClient";
 
 const player = new WHEPClient(
-  "https://customer-nqr1cy6xpbbv13gu.cloudflarestream.com/817583bd076706ef81bc485f7dd4ec58/webRTC/play",
+  "https://live.seanbehan.ca/live/livestream",
   document.querySelector("video")
 );
