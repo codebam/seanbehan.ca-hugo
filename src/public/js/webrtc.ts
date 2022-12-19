57b71506eb4dfe0c83bcc37b5a4ecee22542683f
Sean Behan <contact@seanbehan.dev>
2022-12-19 02:56:37 -0500

    Update webrtc.ts

--- src/public/js/webrtc.ts
+++ src/public/js/webrtc.ts
@@ -3,5 +3,5 @@ import { SrsRtcPlayerAsync } from "./srs.sdk.js";
 const video = document.querySelector("video");
 const player = SrsRtcPlayerAsync();
 window.pc = player.pc;
-player.play("https://live.seanbehan.ca/live/livestream");
+player.play("https://live.codebam.tv/live/livestream");
 video.srcObject = player.stream;
