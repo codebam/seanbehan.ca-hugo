3468a00703be5c2ec0c5e0eff70115cc2df53aa0
Sean Behan <codebam@riseup.net>
2022-12-26 10:16:31 -0500

    fix privacy.ts

--- src/functions/privacy.ts
+++ src/functions/privacy.ts
@@ -1,14 +1,12 @@
-const replaceSite = async (
-  response: Response,
-  context: EventContext<any, any, any>
-) => {
+const replaceSite = async (response: Response, site: string) => {
   let text = await response.text();
-  let site = context.request.url.replace(/\/privacy/g, "");
   text = text.replace(/%SITE%/g, site);
   return new Response(text, { headers: response.headers });
 };
 
-export const onRequestGet = async (context: EventContext<any, any, any>) =>
-  fetch(`${context.request.url}/privacy.txt`).then((response) =>
-    replaceSite(response, context)
+export const onRequestGet = async (context: EventContext<any, any, any>) => {
+  let site = context.request.url.replace(/\/privacy/g, "");
+  return fetch(`${site}/privacy.txt`).then((response) =>
+    replaceSite(response, site)
   );
+};
