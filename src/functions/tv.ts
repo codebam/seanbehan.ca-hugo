6c51eb6096a62d58be431f63a7a144004ee26681
Sean Behan <codebam@riseup.net>
2022-12-26 12:24:30 -0500

    update link to favicon

--- src/functions/tv.ts
+++ src/functions/tv.ts
@@ -1,6 +1,6 @@
 async function modifyPage(response: Response) {
   const head_regex = /\<head\>(\s*[\s\S]*)?(?=\<\/head\>\s*)/gm;
-  const head_tags_subst = `\<head\>$1<meta property="og:title" content="codebam.tv"><meta property="og:site_name" content="codebam.tv"><meta property="og:type" content="video.other"><meta property="og:image" content="https://codebam.tv/favicon.png">`;
+  const head_tags_subst = `\<head\>$1<meta property="og:title" content="codebam.tv"><meta property="og:site_name" content="codebam.tv"><meta property="og:type" content="video.other"><meta property="og:image" content="https://codebam.tv/tv-favicon.png">`;
   const head_adsense_subst = `\<head\>$1<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3287237463323384" crossorigin="anonymous"></script>`;
   const body_regex = /\<body\>(\s*[\s\S]*)?(?=\<\/body\>\s*)/gm;
   const body_adsense_subst = `\<body\>$1<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3287237463323384" crossorigin="anonymous"></script><ins class="adsbygoogle"style="display:block" data-ad-format="autorelaxed" data-ad-client="ca-pub-3287237463323384" data-ad-slot="9333721116"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>`;
